8c469c0ebd4e62407ac52ec133ca4d1044cf72c0
Aashray <aashray@uw.edu>
2018-09-05 13:28:36 -0700

    updated functional tests, to utilize py unittest module, built unittest class with existing test of browser title

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+class visitorTest(unittest.TestCase):
+    # set browser
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    # quit browser    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_list_and_retrieve_later(self):
+        # first, get the webpage
+        self.browser.get('http://localhost:8000')
+        
+        # check browser title is To-Do
+        self.assertIn('To-Do', self.browser.title)
+        
+        # forcing fail to be invoked, can be used to 
+        # output message after all tests passed
+        self.fail('Finish the Test')
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
