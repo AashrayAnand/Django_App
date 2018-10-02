d8cc96254b498912304b4210f5330fda55dd835f
Aashray <aashray@uw.edu>
2018-10-02 00:54:39 -0700

    updated functional test for new visitor to check if header exists, and text is Enter a to-do item, also attempts to input a to-do item into the table, and view if it exists

--- functional_tests.py
+++ functional_tests.py
@@ -18,9 +18,29 @@ class NewVisitorTest(unittest.TestCase):
         # first, get the webpage
         self.browser.get('http://localhost:8000')
         
-        # check browser title is To-Do
+        # notice the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # user is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # user types "Buy peacock features" into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # user hits enter, and the page updates, now the page
+        # lists "1: buy peacock feathers" as an item in
+        # the to-do list table
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         # forcing fail to be invoked, can be used to 
         # output message after all tests passed
         self.fail('Finish the Test')
--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -16,3 +18,13 @@ class HomePageTest(TestCase):
         # checking that the view function found
         # is the home_page view function
         self.assertEqual(found.func, home_page)
+    # test to determine if the root of the site
+    # returns the correct HTML output
+    def test_root_url_returns_correct_html(self):
+        request = HttpRequest()  
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do Lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
