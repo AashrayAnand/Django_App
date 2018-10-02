71b4c17d9573f5f5fc8c6596504d3eac8bc8eb02
Aashray <aashray@uw.edu>
2018-10-02 15:22:59 -0700

    updated view to render python variable as content in a table, updated home_page view function to return a unique response, containing a newly added list item, when the request method type is a POST request

--- lists/tests.py
+++ lists/tests.py
@@ -14,7 +14,14 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+    
     # alternatively, we can create an HttpRequest object, call the
     # home_page view function with the request as parameter, and check
     # the same assertion (self.client.get executes this all in one step) 
+
+    # test to determine if a POST request can be saved
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
